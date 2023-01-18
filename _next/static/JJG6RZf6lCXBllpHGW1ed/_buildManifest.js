0b52ddbadb9c4cb40b31163597ebba1c51ec5e6e
patjawat <patjawat@users.noreply.github.com>
2023-01-18 08:16:54 +0000

    deploy: 6abd8dcc46bd2feaac4e80470768b53279bb7586

---
+++ _next/static/JJG6RZf6lCXBllpHGW1ed/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8d0e95a4647a1b99.css","static/chunks/pages/index-77cf17a5b5bc92d4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
