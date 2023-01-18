4282d1901003754c82fe88dc360c612b4cd9e0b7
patjawat <patjawat@gmail.com>
2023-01-18 16:06:14 +0700

    mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +16,22 @@ export default function Home() {
   return (
     <>
      <h1>Hello สวัสดีครับ</h1>
+     <Button variant="contained">Hello World</Button>
+     <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
+      <Fab disabled aria-label="like">
+        <FavoriteIcon />
+      </Fab>
+    </Box>
     </>
   )
 }
